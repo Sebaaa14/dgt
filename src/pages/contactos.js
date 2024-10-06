e2c2d4abb8892bce9a47f6c8a2cf19f295e7b2f3
Soulhae <benjamin.herrera.a@mail.pucv.cl>
2024-10-06 19:16:50 -0300

    página contactos avance

--- src/pages/contactos.js
+++ src/pages/contactos.js
@@ -1,9 +1,85 @@
-import React from 'react'
+import React from "react";
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "../styles/contactos.css";
 
 const contactos = () => {
   return (
-    <div>contactos</div>
-  )
-}
+    <div className="full-page-background">
+      <Container
+        fluid
+        className="d-flex justify-content-center align-items-center min-vh-100"
+      >
+        <Row className="contact-container">
+          <Col md={6} className="contact-info py-20">
+            <h1>¿Necesitas hablar con nosotros? ¡Contáctanos!</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              eu enim rhoncus ipsum pulvinar gravida. Proin et tempus tortor.
+              Morbi aliquet, lorem ac volutpat luctus, tellus quam viverra ante,
+              quis luctus mi lacus sit amet est. Curabitur id sollicitudin est.
+              Quisque tempor nisl eu dui pellentesque, eget molestie est
+              porttitor.
+            </p>
+          </Col>
+          <Col md={6}>
+            <Form className="contact-form">
+              <Row>
+                <Col md={6}>
+                  <Form.Group controlId="formName" className="mb-2">
+                    <Form.Control
+                      type="text"
+                      placeholder="Nombre"
+                      className="input-field"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group controlId="formEmail" className="mb-2">
+                    <Form.Control
+                      type="email"
+                      placeholder="Email"
+                      className="input-field"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={6}>
+                  <Form.Group controlId="formCompany" className="mb-2">
+                    <Form.Control
+                      type="text"
+                      placeholder="Empresa"
+                      className="input-field"
+                    />
+                  </Form.Group>
+                </Col>
+                <Col md={6}>
+                  <Form.Group controlId="formPhone" className="mb-2">
+                    <Form.Control
+                      type="tel"
+                      placeholder="Teléfono"
+                      className="input-field"
+                    />
+                  </Form.Group>
+                </Col>
+              </Row>
+              <Form.Group controlId="formMessage">
+                <Form.Control
+                  as="textarea"
+                  rows={5}
+                  placeholder="Mensaje"
+                  className="message-field"
+                />
+              </Form.Group>
+              <Button type="submit" className="submit-button mt-3" block>
+                Contactar
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
 
-export default contactos
+export default contactos;
