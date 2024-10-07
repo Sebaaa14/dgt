7c8af7559d79d1f35e05f70ed699290ff6d6f44a
Sebaaa14 <sebazxx14@gmail.com>
2024-10-07 07:56:38 -0300

    footer avanzado

--- src/pages/footer.js
+++ src/pages/footer.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import "../styles/footer.css";
 const footer = () => {
   return (
-    <div>footer</div>
-  )
-}
+    <footer className="footer-container">
+      <Container>
+        <Row className="footer-content">
+          <Col xs={12} md={6}>
+            <p>
+              &copy; {new Date().getFullYear()} Data Guard Technologies. Todos los
+              derechos reservados.
+            </p>
+          </Col>
+          <Col xs={12} md={6} className="footer-links">
+            <a href="/inicio">Inicio</a>
+            <a href="/contactos">Contacto</a>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default footer
+export default footer;
