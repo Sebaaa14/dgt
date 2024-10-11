b7f94a90695959c82be01e43b85ec439c36efb73
Sebaaa14 <sebazxx14@gmail.com>
2024-10-11 01:50:26 -0300

    diseño mejorado

--- src/pages/inicio.js
+++ src/pages/inicio.js
@@ -12,18 +12,28 @@ const Inicio = () => {
           </h1>
         </Col>
       </Row>
-      <Row className="espacio"></Row>
+      <Row className="espacio d-flex align-items-center justify-content-center text-center">
+        <h5>Trabajo colaborativo con más de 10 empresas</h5>
+      </Row>
 
-      <Row className="section section-2 d-flex align-items-center justify-content-center text-center">
-        <Col md={8}>
-          <h3 className="text-white">Fuerte énfasis en innovación</h3>
+      <Row className="section section-2 d-flex align-items-center justify-content-center text-center py-5">
+        <Col md={5} className="text-container-row2">
+          <h2 className="texto2">Fuerte énfasis en innovación</h2>
           <p className="text-white">
             Nos hemos posicionado como una de las empresas TI más destacadas en
             innovación durante todo el año 2024.
           </p>
         </Col>
+        <Col
+          md={5}
+          className="fila2col2 d-flex justify-content-center align-items-center"
+        >
+          <div className="image-container"></div>
+        </Col>
       </Row>
-      <Row className="espacio"></Row>
+      <Row className="espacio d-flex align-items-center justify-content-center text-center">
+        <h5>Número 1 en gestión de relaciones</h5>
+      </Row>
 
       <Row className="section section-3 d-flex align-items-center justify-content-center text-center">
         <Col md={8}>
